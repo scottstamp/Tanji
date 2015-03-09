79cdbca7b4ec52f651fb58b13ec55d6b2bfd5274
ArachisH <TheOldNutman@gmail.com>
2015-03-09 12:27:51 -0700

    Added serializable class to handle tanji's settings.

---
+++ Tanji/Utilities/TanjiSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Tanji.Utilities
+{
+    [Serializable]
+    public class TanjiSettings
+    {
+        private static readonly XmlSerializer _xmlSerializer;
+        private const string TANJI_SETTINGS_FILENAME = "tanji_settings.xml";
+
+        private static TanjiSettings _settings;
+        public static TanjiSettings Global
+        {
+            get { return _settings; }
+        }
+
+        public bool IsAlwaysOnTop { get; set; }
+        public bool IsEdgeSnappingEnabled { get; set; }
+        public bool ShouldCloseOnDisconnect { get; set; }
+
+        static TanjiSettings()
+        {
+            _settings = new TanjiSettings();
+            _xmlSerializer = new XmlSerializer(Global.GetType());
+        }
+
+        public static void Save()
+        {
+            using (var textWriter = new StreamWriter(TANJI_SETTINGS_FILENAME))
+                _xmlSerializer.Serialize(textWriter, Global);
+        }
+        public static void Load()
+        {
+            if (!File.Exists(TANJI_SETTINGS_FILENAME)) return;
+            using (var stream = new FileStream(TANJI_SETTINGS_FILENAME, FileMode.Open, FileAccess.Read, FileShare.Read))
+                _settings = (TanjiSettings)_xmlSerializer.Deserialize(stream);
+        }
+    }
+}
